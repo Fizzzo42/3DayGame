68803d8f5a0f52f5156be5c4ddbbf23e960671e4
Fizzzo <Fizzzo42@gmail.com>
2023-07-18 12:05:35 +0200

    collision with enemies and player

--- scenes/enemy.gd
+++ scenes/enemy.gd
@@ -2,6 +2,9 @@ extends CharacterBody2D
 
 const MAX_SPEED = 50
 
+func _ready():
+	$Area2D.body_entered.connect(on_body_entered)
+
 func _process(delta):
 	var direction = get_direction_to_player()
 	velocity = direction * MAX_SPEED
@@ -9,7 +12,24 @@ func _process(delta):
 
 
 func get_direction_to_player():
-	var player_node = get_tree().get_first_node_in_group("player") as Node2D
+	return (get_player_global_position() - global_position).normalized()
+
+
+func on_body_entered(other_area: Node2D):
+	var player_position = get_player_global_position()
+	var propel_direction = player_position - global_position
+	
+	var player_node = get_player() as CharacterBody2D
+	player_node.velocity = propel_direction * 50
+	
+	queue_free()
+
+func get_player_global_position():
+	var player_node = get_player() as Node2D
 	if player_node != null:
-		return (player_node.global_position - global_position).normalized()
+		return player_node.global_position
 	return Vector2.ZERO
+
+
+func get_player():
+	return get_tree().get_first_node_in_group("player")
--- scenes/player.gd
+++ scenes/player.gd
@@ -1,16 +1,16 @@
 extends CharacterBody2D
 
 const MAX_SPEED = 140
-const ACCELERATION_SMOOTHING = 30 #higher = less smooth
+const ACCELERATION_SMOOTHING = 10 #lower = smoother
+
 
 func _process(delta):
 	var movement_vector = get_movement_vector()
 	var direction = movement_vector.normalized()
 	var target_velocity = direction * MAX_SPEED
-	
 	velocity = velocity.lerp(target_velocity, 1 - exp(-delta * ACCELERATION_SMOOTHING))
-	
 	move_and_slide()
+
 
 func get_movement_vector():
 	var x_movement = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
